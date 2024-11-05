bc2a77fdaa62a717ed54710108fbca3e4bf04ae7
Ricardo <ric_msouza@hotmail.com>
2024-11-05 09:44:01 -0300

    tornando mais segura as chaves de acesso

--- Aula-06/autenticacao/src/firebase/config.jsx
+++ Aula-06/autenticacao/src/firebase/config.jsx
@@ -1,17 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCLTy9Q_YjWuApU83vrLIR2PODqNLREmLU",
-  authDomain: "authentication-67166.firebaseapp.com",
-  projectId: "authentication-67166",
-  storageBucket: "authentication-67166.firebasestorage.app",
-  messagingSenderId: "1073464605003",
-  appId: "1:1073464605003:web:2eb85e6dc2bfdc671490ce",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
